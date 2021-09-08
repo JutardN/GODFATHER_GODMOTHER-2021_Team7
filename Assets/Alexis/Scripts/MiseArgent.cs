db9892cfbee8ef3ffe5edbb969bdb2156fbfb46b
Framby21 <aalexy.hubert@gmail.com>
2021-09-08 14:41:59 +0200

    Error repaired

---
+++ Assets/Alexis/Scripts/MiseArgent.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MiseArgent : MonoBehaviour
+{
+
+    public Text moneySpend;
+
+    public Text prixDepart;
+    public Text miseActuelle;
+    public Text maMise;
+
+    public int prixDepartMoney;
+    public int MiseActuelleMoney;
+    public int maMiseMoney;
+
+    public int coeffMoney;
+    int moneyValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        prixDepart.text = "" + prixDepartMoney;
+        miseActuelle.text = "" + MiseActuelleMoney;
+        maMise.text = "" + maMiseMoney;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void addMoney()
+    {
+
+        moneyValue += coeffMoney;
+        moneySpend.text = "" + moneyValue;
+
+    }
+    public void subMoney()
+    {
+        if (moneyValue > 0)
+        {
+            moneyValue -= coeffMoney;
+            moneySpend.text = "" + moneyValue;
+        }
+    }
+
+    public void ValiderMoney()
+    {
+        maMiseMoney = moneyValue;
+        maMise.text = "" + maMiseMoney;
+    }
+    public void AnnulerMoney()
+    {
+        maMiseMoney = 0;
+        moneyValue = 0;
+        maMise.text = "" + maMiseMoney;
+        moneySpend.text = "" + moneyValue;
+    }
+}
